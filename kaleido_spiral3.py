c3d26e69e57846b014298ffd70ffd8ace95228a7
aruon28 <134406660+aruon28@users.noreply.github.com>
2023-06-27 16:59:24 +0530

    Update kaleido_spiral3.py

--- kaleido_spiral3.py
+++ kaleido_spiral3.py
@@ -3,8 +3,7 @@ import turtle
 from itertools import cycle
 colors = cycle(['red', 'orange', 'yellow', 'green', 'blue', 'purple'])
 
-
-def draw_circle(size, angle, shift):
+def draw_circle(size,angle,shift):
     turtle.bgcolor(next(colors))
     turtle.pencolor(next(colors))
     turtle.circle(size)
